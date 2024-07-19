745354cf7139b46e13a8d9baa5ea6abb8694a567
John <johnjo1116@gmail.com>
2024-07-19 17:07:48 +0900

    Calculation Done

--- bodyInfo/bodyCalculate/urls.py
+++ bodyInfo/bodyCalculate/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import calculate_metrics
 
 urlpatterns = [
-    path('calculate/', calculate_metrics, name='calculate_metrics'),
+    path('', calculate_metrics, name='calculate_metrics'),
 ]
